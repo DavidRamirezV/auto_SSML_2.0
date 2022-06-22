0d13bff71bf6430e6a08fcb9e1d221700c3e9d66
David Ramirez <david.ramirez.v@usach.cl>
2022-06-22 19:17:06 -0400

    API REST
    sintetizar voz mediante servicio api rst

--- src/functions/make_ssml.go
+++ src/functions/make_ssml.go
@@ -1,8 +1,15 @@
 package functions
 
+import (
+	"fmt"
+)
+
 func MakeSSML(text string) string {
 
 	output := BreakBeginingEnd(text)
+
+	//print final ssml
+	fmt.Printf("\n%v\n\n", output)
 
 	return output
 }
